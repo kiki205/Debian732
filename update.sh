0d7169ee61a1006e2fa37decdc08fd7cc5d05871
Mbah Shondong <l4nangproduction@gmail.com>
2017-09-28 09:08:16 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,52 +16,29 @@ if [[ $USER != 'root' ]]; then
 	exit
 fi
 
-MYIP=$(wget -qO- ipv4.icanhazip.com)
-
-#vps="iqbalfaf";
-vps="vps";
-
-if [[ $vps = "iqbalfaf" ]]; then
-	source="http://deb.mbahshondong.com"
-else
-	source="http://deb.mbahshondong.com"
-fi
-
 # go to root
 cd
 
-# check registered ip
-wget -q -O IP $source/IP.txt
-if ! grep -w -q $MYIP IP; then
-	echo -e $yellow"Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini"
-	if [[ $vps = "iqbalfaf" ]]; then
-		echo "Hubungi: Mbah Shondong Daftar IP Free"
-	else
-		echo -e $white"SMS 082322649802 https://facebook.com/ngamalembahe"
-	fi
-	rm -f /root/IP
-	exit
-fi
-
-echo -e $yellow "[ ! ]  Updating Script VPS.....  "
-wget -O /usr/bin/dropmon "http://deb.mbahshondong.com/Debian7/dropmon.sh" >> $log 2>&1
-wget -O /usr/bin/menu "http://deb.mbahshondong.com/Debian7/menu.sh" >> $log 2>&1
-wget -O /usr/bin/user-add "http://deb.mbahshondong.com/Debian7/user-add.sh" >> $log 2>&1
-wget -O /usr/bin/user-add-vpn "http://deb.mbahshondong.com/Debian7/user-add-vpn.sh" >> $log 2>&1
-wget -O /usr/bin/user-add-pptp "http://deb.mbahshondong.com/Debian7/user-add-pptp.sh" >> $log 2>&1
-wget -O /usr/bin/user-expire "http://deb.mbahshondong.com/Debian7/user-expire.sh" >> $log 2>&1
-wget -O /usr/bin/user-gen "http://deb.mbahshondong.com/Debian7/user-gen.sh" >> $log 2>&1
-wget -O /usr/bin/user-limit "http://deb.mbahshondong.com/Debian7/user-limit.sh" >> $log 2>&1
-wget -O /usr/bin/user-list "http://deb.mbahshondong.com/Debian7/user-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-login "http://deb.mbahshondong.com/Debian7/user-login.sh" >> $log 2>&1
-wget -O /usr/bin/user-active-list "http://deb.mbahshondong.com/Debian7/user-active-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-renew "http://deb.mbahshondong.com/Debian7/user-renew.sh" >> $log 2>&1
-wget -O /usr/bin/user-del "http://deb.mbahshondong.com/Debian7/user-del.sh" >> $log 2>&1
-wget -O /usr/bin/user-pass "http://deb.mbahshondong.com/Debian7/user-pass.sh" >> $log 2>&1
-wget -O /usr/bin/user-expire-list "http://deb.mbahshondong.com/Debian7/user-expire-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-banned "http://deb.mbahshondong.com/Debian7/user-banned.sh" >> $log 2>&1
-wget -O /usr/bin/unbanned-user "http://deb.mbahshondong.com/Debian7/unbanned-user.sh" >> $log 2>&1
-wget -O /usr/bin/delete-user-expire "http://deb.mbahshondong.com/Debian7/delete-user-expire.sh" >> $log 2>&1
+echo -e $yellow "[ ! ]  Proses Updating Script VPS .....  "
+echo -e $yellow "[ ! ]  ................     Please wait  "
+wget -O /usr/bin/dropmon "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/dropmon.sh" >> $log 2>&1
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/menu.sh" >> $log 2>&1
+wget -O /usr/bin/user-add "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add.sh" >> $log 2>&1
+wget -O /usr/bin/user-add-vpn "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add-vpn.sh" >> $log 2>&1
+wget -O /usr/bin/user-add-pptp "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add-pptp.sh" >> $log 2>&1
+wget -O /usr/bin/user-expire "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-expire.sh" >> $log 2>&1
+wget -O /usr/bin/user-gen "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-gen.sh" >> $log 2>&1
+wget -O /usr/bin/user-limit "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-limit.sh" >> $log 2>&1
+wget -O /usr/bin/user-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-login "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-login.sh" >> $log 2>&1
+wget -O /usr/bin/user-active-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-active-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-renew "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-renew.sh" >> $log 2>&1
+wget -O /usr/bin/user-del "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-del.sh" >> $log 2>&1
+wget -O /usr/bin/user-pass "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-pass.sh" >> $log 2>&1
+wget -O /usr/bin/user-expire-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-expire-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-banned "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-banned.sh" >> $log 2>&1
+wget -O /usr/bin/unbanned-user "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/unbanned-user.sh" >> $log 2>&1
+wget -O /usr/bin/delete-user-expire "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/delete-user-expire.sh" >> $log 2>&1
 
 chmod +x /usr/bin/dropmon >> $log 2>&1
 chmod +x /usr/bin/menu >> $log 2>&1
