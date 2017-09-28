528a09f917910cd024593b3c5bd0c73b3a451fe4
Mbah Shondong <l4nangproduction@gmail.com>
2017-09-28 08:21:40 +0700

    Update pptp.sh

--- Debian7/pptp.sh
+++ Debian7/pptp.sh
@@ -11,13 +11,10 @@ BlueF='\e[1;34m'
 # go to root
 cd
 
-
 # get the VPS IP
 #ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 ip=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
-if [ "$ip" = "" ]; then
-	ip=$(wget -qO- ipv4.icanhazip.com)
-fi
+
 
 echo ""
 echo -e $green""
