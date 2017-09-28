a1c31e460e374433dd8f27487d730112597c7174
Mbah Shondong <l4nangproduction@gmail.com>
2017-09-28 08:31:28 +0700

    Update user-renew.sh

--- Debian7/user-renew.sh
+++ Debian7/user-renew.sh
@@ -8,37 +8,8 @@ red='\e[1;31m'
 yellow='\e[1;33m'
 BlueF='\e[1;34m'
 
-if [[ $USER != 'root' ]]; then
-	echo -e $yellow"Maaf, Anda harus menjalankan ini sebagai root"
-	exit
-fi
-
-MYIP=$(wget -qO- ipv4.icanhazip.com)
-
-#vps="iqbalfaf";
-vps="vps";
-
-if [[ $vps = "iqbalfaf" ]]; then
-	source="http://deb.mbahshondong.com"
-else
-	source="http://deb.mbahshondong.com"
-fi
-
 # go to root
 cd
-
-# check registered ip
-wget -q -O IP $source/IP.txt
-if ! grep -w -q $MYIP IP; then
-	echo -e $yellow"Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini"
-	if [[ $vps = "iqbalfaf" ]]; then
-		echo "Hubungi: Mbah Shondong Daftar IP Free"
-	else
-		echo -e $white"SMS 082322649802 https://facebook.com/ngamalembahe"
-	fi
-	rm -f /root/IP
-	exit
-fi
 
 echo -e $yellow""
 echo "-----------TAMBAH MASA AKTIF AKUN SSH/VPN -------------"
@@ -104,4 +75,3 @@ else
 fi
 
 cd ~/
-rm -f /root/IP
