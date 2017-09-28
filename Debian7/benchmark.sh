2c5f3db45bd25591d3d8ff158facc51eb9e8db74
Mbah Shondong <l4nangproduction@gmail.com>
2017-09-28 08:02:31 +0700

    Update benchmark.sh

--- Debian7/benchmark.sh
+++ Debian7/benchmark.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-#vps="iqbalfaf";
-vps="vps";
-
-if [[ $vps = "iqbalfaf" ]]; then
-	source="http://deb.mbahshondong.com"
-else
-	source="http://deb.mbahshondong.com"
-fi
 
 # go to root
 cd
 
-wget http://deb.mbahshondong.com/Debian7/bench.sh -O - -o /dev/null|bash
+wget https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/bench.sh -O - -o /dev/null|bash
